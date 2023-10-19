c6f2047a75d9c74097ede1f5797dc599ff77ba19
Jun Hong Choo <jh.choo0902@gmail.com>
2023-10-19 13:55:12 +0800

    Update run.sh

--- rhsso/v7.6.0/run.sh
+++ rhsso/v7.6.0/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -d /opt/rh/rh-sso-7.6/standalone/data ]; then
-  /opt/rh/rh-sso-7.6/bin/init.d/jboss-eap-rhel.sh
+  tail -f /opt/rh/rh-sso-7.6/standalone/logs/server.log #/opt/rh/rh-sso-7.6/bin/init.d/jboss-eap-rhel.sh
 else
   cp -r /tmp/rh /opt/ && /opt/rh/rh-sso-7.6/bin/standalone.sh -b 0.0.0.0
 fi
