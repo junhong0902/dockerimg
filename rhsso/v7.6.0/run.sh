9f7f3656a11816a4a3f480476c97d39a282aeb33
Jun Hong Choo <jh.choo0902@gmail.com>
2023-10-19 14:18:55 +0800

    Update run.sh

--- rhsso/v7.6.0/run.sh
+++ rhsso/v7.6.0/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -d /opt/rh/rh-sso-7.6/standalone/data ]; then
-  /opt/rh/rh-sso-7.6/bin/standalone.sh -b 0.0.0.0
+  /opt/rh/rh-sso-7.6/bin/standalone.sh -b 0.0.0.0 # so it does not replace the installation file
 else
   cp -r /tmp/rh /opt/ && /opt/rh/rh-sso-7.6/bin/standalone.sh -b 0.0.0.0
 fi
